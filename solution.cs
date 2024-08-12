c4a111d2dd51674dfbd500cba65d33385ee14f40
Kooreis <kooreis.official@gmail.com>
2024-08-12 15:36:29 -0500

    This update defines the function minDistance and initializes the 2D array dp. The dimensions of dp are determined by the lengths of the input words.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a JavaScript solution for the problem:
-
-```javascript
 function minDistance(word1, word2) {
     var len1 = word1.length;
     var len2 = word2.length;
@@ -9,29 +6,4 @@ function minDistance(word1, word2) {
     for (var i = 0; i < len1 + 1; i++) {
         dp[i] = new Array(len2 + 1);
     }
-
-    for (var i = 0; i <= len1; i++) {
-        dp[i][0] = i;
-    }
-
-    for (var j = 0; j <= len2; j++) {
-        dp[0][j] = j;
-    }
-
-    for (var i = 1; i <= len1; i++) {
-        for (var j = 1; j <= len2; j++) {
-            if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
-            }
-        }
-    }
-
-    return dp[len1][len2];
 }
-
-console.log(minDistance('kitten', 'sitting'));
-```
-
-This console application computes the edit distance between two words. The edit distance is the minimum number of operations (insertions, deletions, and substitutions) required to transform one word into another. The application uses dynamic programming to solve the problem. It creates a 2D array where dp[i][j] represents the minimum number of operations required to convert word1[0..i-1] to word2[0..j-1]. The final result is stored in dp[len1][len2].
